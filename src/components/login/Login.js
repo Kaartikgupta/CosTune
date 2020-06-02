5c7550459f8b136d7d7ef562a6cab7300c09cc03
Kaartik <kaartik.1rn17cs041@gmail.com>
2020-06-02 22:36:43 +0530

    update

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -1,14 +1,86 @@
 import React from "react";
+import Popup from "reactjs-popup";
+import "./login.css";
+class ControlledPopup extends React.Component {
+            constructor(props) {
+                super(props);
+                this.state = { open: false };
+                this.openModal = this.openModal.bind(this);
+                this.closeModal = this.closeModal.bind(this);
+                this.state={
+                    username:"",
+                    password:"",
+                }
+                this.handleSubmit=this.handleSubmit.bind(this)
+            }
+            openModal() {
+            this.setState({ open: true });
+            }
+            closeModal() {
+            this.setState({ open: false });
+            }
+    
+            namehandler=(event)=>{
+                this.setState({
+                    username: event.target.value
+                })
+            }
+            passwordhandler=(event)=>{
+                this.setState({
+                    password: event.target.value
+                })
+            }
+            handleSubmit=(event)=>{
+                
+                console.log(this.state);
+                this.setState({
+                    username:"",
+                    password:"",
+                })
+            event.preventDefault()
+            }
+        
+        
+            render() {
+            return (
+                <div>
+                <button className="button" onClick={this.openModal}>
+                    Login
+                </button>
+                <Popup
+                    open={this.state.open}
+                    closeOnDocumentClick
+                    onClose={this.closeModal}
+                >
+                    <div className="box">
+                        <div className="leftbox">
+                        <div className="black" />
+                        <div className="image" />
+                        <div className="innerbox">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="text"><h4><span>LOGIN</span></h4>
+                                    <div className="inputbox">
+                                        <input className="inputs" id="username" type="text" value={this.state.username} onChange={this.namehandler}placeholder="Username" />
+                                    </div>
+                                    <div className="inputbox">
+                                        <input className="inputs" id="password" type="password" value={this.state.password} onChange={this.passwordhandler} placeholder="Password" />
+                                    </div>
+                                    <input className="btn" type="submit" value="Submit" />
+                                    <div className="othertext">
+                                        Haven't Registered Yet? Register
+                                    </div>
+                                </div>
+                            </form>
+                        </div>
+                        </div>
+                    </div>
+                    
+                </Popup>
+                </div>
+            );
+    }
+  }
+  export default ControlledPopup;
 
-import "../../style.css";
 
-function Login() {
-  return (
-    <div>
-      <h2>Login page</h2>
-      <p>Login</p>
-    </div>
-  );
-}
-
-export default Login;
+// export default Login;
