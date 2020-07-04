4b907f6654a0010e26c9e2b76415cbe4b786c4af
Kaartik <kaartik.1rn17cs041@gmail.com>
2020-07-04 18:37:54 +0530

    added logout

---
+++ src/AppLogout.js
@@ -0,0 +1,42 @@
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+import Layout from "./components/common/Layout";
+import NavigationBarLogout from "./components/common/NavigationBarLogout";
+import Home from "./components/landing/Home";
+import About from "./components/about/About";
+import Music from "./components/player/Player";
+import Login from "./components/login/Login";
+import Signup from "./components/signup/Signup";
+import Contact from "./components/contact/Contact";
+import Footer from "./components/common/Footer";
+document.body.style = "background: #1f2022;";
+function App() {
+  return (
+    <React.Fragment>
+      <Router>
+        <NavigationBarLogout />
+        <Layout>
+          <Switch>
+            <Route path="/" exact component={Home} />
+            <Route path="/about" component={About} />
+            <Route path="/player" component={Music} />
+            <Route path="/contact" component={Contact} />
+            <Route path="/login" component={Login} />
+            <Route path="/signup" component={Signup} />
+            <Redirect from="*" to="/"></Redirect>
+          </Switch>
+        </Layout>
+      </Router>
+    </React.Fragment>
+  );
+}
+
+export default App;
